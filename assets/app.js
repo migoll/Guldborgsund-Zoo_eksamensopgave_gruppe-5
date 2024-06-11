79c58457feebb92fffb6e9da06a4baea81f2e863
Nathalia1111 <148855345+Nathalia1111@users.noreply.github.com>
2024-06-11 09:55:43 +0200

    Forsøgte at hente vores dyr posts fra wordpress, men det lykkedes ikke

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,5 @@
 const url = "https://guldborgsundzoo.nathaliawd.com/wp-json/wp/v2/posts";
+const containerEl = document.querySelector('.animal-display');
 
 fetch(url)
 .then (res => res.json())
@@ -7,15 +8,54 @@ fetch(url)
 })
 .catch (err => {console.log("Fejl:", err)})
 
-export function renderAnimal(data, containerEl){
-  containerEl.innerHTML = "";
+function animalDisplay(data){
+  data.forEach(post => {
+    const animalContainer = document.createElement('div');
+    const animalTitle = document.createElement('h3');
+    const animalImage = document.createElement('img');
 
-  data.forEach((data) => {
+    animalTitle.textContent = post.title.rendered;
     
-    const animalEl = document.createElement("a");
-    const titleEl = document.createElement("h3");
 
-    animalEl.href = "/enkelt-dyr.html?id=" + data.id;
+    const htmlContent = post.content.rendered;
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(htmlContent, 'text/html');
+    const imgElement = doc.querySelector('img');
+    const imageUrl = imgElement ? imgElement.getAttribute('src') : '';
 
+    animalImage.src = imageUrl;
+
+
+    animalContainer.append(animalTitle, animalImage);
+
+    animalContainer.addEventListener('click', () => {
+      window.location.href = `details.html?id=${post.id}`;
+    });
+
+    containerEl.append(animalContainer);
   })
 }
+
+
+
+// const url = "https://guldborgsundzoo.nathaliawd.com/wp-json/wp/v2/posts";
+
+// fetch(url)
+// .then (res => res.json())
+// .then (posts =>{
+//   displayPosts(posts);
+// })
+// .catch (err => {console.log("Fejl:", err)})
+
+// export function renderAnimal(data, containerEl){
+//   containerEl.innerHTML = "";
+
+//   data.forEach((data) => {
+    
+//     const animalEl = document.createElement("a");
+//     const titleEl = document.createElement("h3");
+
+//     animalEl.href = "/enkelt-dyr.html?id=" + data.id;
+
+//   })
+// }
